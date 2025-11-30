2a4c6e00aefe29cc57e2a2e8f492e8b95306d340
Aly <aly.kazani52@gmail.com>
2025-11-30 22:10:27 +0500

    minor changes

--- Algorithms/rightrotate_Tree.cpp
+++ Algorithms/rightrotate_Tree.cpp
@@ -12,29 +12,5 @@ class TreeNode
 
 TreeNode* rightRotate(TreeNode* oldParent) 
 {
-
-// 1. detach left child's right subtree
-
-TreeNode* orphan = oldParent->left->right;
-
-// 2. consider left child to be the new parent
-
-TreeNode* newParent = oldParent->left;
-
-// 3. attach old parent onto right of new parent
-
-newParent->right = oldParent;
-
-// 4. attach new parent's old right subtree as
-
-// left subtree of old parent
-
-oldParent->left = orphan;
-
-oldParent->height = height(oldParent); // update nodes'
-
-newParent.height = height(newParent); // height values
-
-return newParent;
-
+    // TODO: Update Code for AVL Trees
 }
